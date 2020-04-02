40a70e3087c72a2c3e7d7b7ae0dafc229ea3f2b2
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-04-02 00:41:16 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,21 +1,21 @@
 def find_min_in_nested_arrays(src)
-  find_greater_pair = []
+  outer_results = []
   row_index = 0
   while row_index < src.count do
     element_index = 0
     longest_string_element = ""
     while element_index < src [row_index].count do
 
-      if src[row_index][element_index]> longest_string_element
+      if src[row_index][element_index] < longest_string_element
         longest_string_element = src[row_index][element_index]
       end
       element_index += 1
     end
 
-    find_greater_pair << longest_string_element
+    outer_results << longest_string_element
     row_index += 1
   end
 
-  find_greater_pair
+  outer_results
 
 end
